1c72c2c3c2dd11cf9e0f418d660a030b93238b2b
Ruslan <ruslangazizov36@gmail.com>
2021-12-02 14:54:56 +0300

    Add audio button for cells in tables

--- 007-011_2021/Controllers/Words/WordsViewController.swift
+++ 007-011_2021/Controllers/Words/WordsViewController.swift
@@ -9,36 +9,39 @@ import UIKit
 
 class WordsViewController: UIViewController {
 
-    // MARK: - Properties
-    var words: [Word] = []
-    let interactor = Interactor()
-    
-    // MARK: - Table view builder
-    func createWordsTableView() -> UITableView {
-        let tableView = UITableView()
-        tableView.rowHeight = 40
-        
-        tableView.register(WordsTableViewCell.self,
-                           forCellReuseIdentifier: WordsTableViewCell.reuseIdentifier)
-        
+    // MARK: - UI
+    lazy final var searchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.delegate = self
         searchBar.placeholder = "Enter word"
         searchBar.sizeToFit()
+        return searchBar
+    }()
+
+    // MARK: - Properties
+    final var words: [Word] = []
+    final let interactor = Interactor()
+
+    // MARK: - Table view builder
+    final func createWordsTableView() -> UITableView {
+        let tableView = UITableView()
+        tableView.rowHeight = 40
         tableView.tableHeaderView = searchBar
-        
+        tableView.register(WordsTableViewCell.self,
+                           forCellReuseIdentifier: WordsTableViewCell.reuseIdentifier)
         return tableView
     }
-    
+
     // MARK: - Layout methods
     func configureLayout(for tableView: UITableView) {
         view.addSubview(tableView)
-        
+        tableView.addSubview(searchBar)
+
         tableView.snp.makeConstraints { make in
             make.left.top.right.equalTo(view.safeAreaLayoutGuide).inset(5)
             make.bottom.equalTo(view.safeAreaLayoutGuide)
         }
-        tableView.tableHeaderView?.snp.makeConstraints { make in
+        searchBar.snp.makeConstraints { make in
             make.height.equalTo(50)
             make.width.equalTo(tableView.snp.width)
         }
@@ -52,7 +55,7 @@ extension WordsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return words.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(
             withIdentifier: WordsTableViewCell.reuseIdentifier,
@@ -62,7 +65,8 @@ extension WordsViewController: UITableViewDataSource {
         }
         let cellData = WordsTableCellData(
             word: words[indexPath.row].word,
-            phonetic: words[indexPath.row].phonetic
+            phonetic: words[indexPath.row].phonetic,
+            audioURL: words[indexPath.row].phonetics.first?.audio
         )
         cell.configure(with: cellData)
         return cell
@@ -76,10 +80,11 @@ extension WordsViewController: UISearchBarDelegate {
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         searchBar.showsCancelButton = true
     }
-    
+
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.endEditing(true)
         searchBar.showsCancelButton = false
     }
 
 }
+
---
+++ 007-011_2021/Services/Protocols/AudioPlayerProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  AudioPlayerProtocol.swift
+//  007-011_2021
+//
+//  Created by Руслан on 02.12.2021.
+//
+
+import Foundation
+
+protocol AudioPlayerProtocol {
+
+    func playAudio(using audioURL: String)
+
+}
