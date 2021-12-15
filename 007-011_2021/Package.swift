a178b7476e9aa59b3571116e8a0f3ceff63de6f9
belyab <72295958+belyab@users.noreply.github.com>
2021-12-16 02:28:02 +0300

    Update Package.swift

--- 007-011_2021/Package.swift
+++ 007-011_2021/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "007-011_2021",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/magicalpanda/MagicalRecord",
+         .package(url: "https://github.com/magicalpanda/MagicalRecord")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
